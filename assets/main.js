bd827d1344e3059a8adb57d9a02726a9c9ffccf3
Chase Pritchett <chasepritchett@Chases-MacBook-Pro-2.local>
2018-03-17 10:24:41 -0500

    fixed assets

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + moodInput + "&api_key=H8YEjrprBP114UoKPFK4mv1xKW8sKR6o";
+var moodInput = "";
+
+var horoscopeURL = "http://horoscope-api.herokuapp.com/horoscope/today/" + horoscopeSign;
+var horoscopeSign = "";
