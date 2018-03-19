e953ebf8c03e09126822d41c79f6eaaab86ec681
Ktlewis17 <klewis.dpt@gmail.com>
2018-03-19 18:59:20 -0500

    updated index

--- assets/main.js
+++ assets/main.js
@@ -13,7 +13,9 @@ $(document).ready(function () {
 
         $("#modal1").hide();
 
+
         //end of modal fadout function
+
     });
 
 
@@ -40,9 +42,43 @@ $(document).ready(function () {
             $("#giph-results").html(response);
         });
     });
-
-
 });
+
+
+// To-do List on Home Page //
+
+
+$(document).ready(function() {
+    $(function() {
+      $('#calendar').fullCalendar({
+        defaultView: 'listWeek',
+        views: {
+          listDay: { buttonText: 'list day' },
+          listWeek: { buttonText: 'list week' },
+          listMonth: { buttonText: 'list month' }
+        },
+        header: {
+          left: 'title',
+          center: '',
+          right: 'listDay,listWeek,listMonth'
+        },
+        events: 'https://fullcalendar.io/demo-events.json'
+      })
+    });
+    eventRender: function(event, element, start, end, timezone, callback) {
+      element.qtip({
+        content: event.description
+        startEditable: true;
+                   });
+                                          });
+              });
+      });
+
+
+
+
+
+
 
 //Stats Page
 //Plotly Pie Chart
@@ -52,11 +88,7 @@ var data = [{
     type: "pie"
 }];
 
-<<<<<<< HEAD
-    // Plotly.newPlot("myDivPie", data);
-=======
 //Plotly.newPlot("myDivPie", data);
->>>>>>> 75488c6b40c46023a7bf2fcf5866469ead771221
 
 //Plotly Bubble Chart
 var trace1 = {
@@ -82,134 +114,3 @@ var layout = {
 
 
 
-<<<<<<< HEAD
-    // Plotly.newPlot("myDiv", data, layout);
-  
-// To-do List Main Page //
-    $(document).ready(function(){
-
-        $(function() {
-            $("calendar").fullCalendar({
-              defaultView: "listWeek",
-          
-              // customize the button names,
-              // otherwise they'd all just say "list"
-              views: {
-                listDay: { buttonText: "list day" },
-                listWeek: { buttonText: "list week" },
-                listMonth: { buttonText: "list month" }
-                     },
-          
-              header: {
-                left: "title",
-                center: "",
-                right: "listDay,listWeek,listMonth"
-                      },
-              events: "https://fullcalendar.io/demo-events.json"
-                                         });
-          });
-        });
-          
-
-
-=======
-// To-do List on Home Page //{
-
-
-    $(document).ready(function(){
-
-     var todoList = document.createElement("fullcalendar");
-     list.addEventListener('click', function() { 
-         console.log(todoList)
-            $("#calendar").fullCalendar({
-              defaultView: "listWeek",
-          
-              // customize the button names,
-              // otherwise they'd all just say "list"
-              views: {
-                listDay: { buttonText: "list day" },
-                listWeek: { buttonText: "list week" },
-                listMonth: { buttonText: "list month" }
-                     },
-          
-              header: {
-                left: "title",
-                center: "",
-                right: "listDay,listWeek,listMonth"
-                      },
-              events: "https://fullcalendar.io/demo-events.json"
-                                         });
-          });
-        });
-          
->>>>>>> 75488c6b40c46023a7bf2fcf5866469ead771221
-
-        // trigger events on calendar
-        $('#calendar').fullCalendar({
-            eventClick: function(calEvent, jsEvent, view) {
-<<<<<<< HEAD
-=======
-                console.log(calendar)
->>>>>>> 75488c6b40c46023a7bf2fcf5866469ead771221
-          
-              alert('Event: ' + calEvent.title);
-              alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-              alert('View: ' + view.name);
-          
-              // change the border color just for fun
-              $(this).css('border-color', 'red');
-          
-            }
-          });
-
-          $('#calendar').fullCalendar({
-            events: [
-              {
-                title: 'My Event',
-                start: '2010-01-01',
-                url: 'http://google.com/'
-              }
-              // other events here
-            ],
-            eventClick: function(event) {
-              if (event.url) {
-                window.open(event.url);
-                return false;
-              }
-            }
-          });
-
-
-        // Full Calendar on Calendar Page//
-        $(function() {
-
-            $('#calendar').fullCalendar({
-              eventClick: function(eventObj) {
-                if (eventObj.url) {
-                  alert(
-                    'Clicked ' + eventObj.title + '.\n' +
-                    'Will open ' + eventObj.url + ' in a new tab'
-                  );
-          
-                  window.open(eventObj.url);
-          
-                  return false; // prevents browser from following link in current tab.
-                } else {
-                  alert('Clicked ' + eventObj.title);
-                }
-              },
-              defaultDate: '2018-03-15',
-              events: [
-                {
-                  title: 'simple event',
-                  start: '2018-03-02'
-                },
-                {
-                  title: 'event with URL',
-                  url: 'https://www.google.com/',
-                  start: '2018-03-03'
-                }
-              ]
-            });
-          
-          });
