4855c6b274d682c517832e72840bbe40e3b1bf48
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-03-19 10:47:38 -0500

    update

--- assets/main.js
+++ assets/main.js
@@ -1,32 +1,56 @@
 //Stats Page
-    //Plotly Pie Chart
-    var data = [
-    {
-        values: [19, 26, 55],
-        labels: ["Residential", "Non-Residential", "Utility"],
-        type: "pie"
-    }
-    ];
 
-    Plotly.newPlot("myDivPie", data);
+//*********************Begin Chart Input**********************
 
-    //Plotly Bubble Chart
-    var trace1 = {
-    x: [1, 2, 3, 4],
-    y: [10, 11, 12, 13],
-    mode: "markers",
-    marker: {
-        size: [40, 60, 80, 100]
-    }
-    };
+    //Plotly Pie Chart Start
+        var data = [
+        {
+            values: [19, 26, 55],
+            labels: ["Residential", "Non-Residential", "Utility"],
+            type: "pie"
+        }
+        ];
 
-    var data = [trace1];
+        Plotly.newPlot("myDivPie", data);
+    //Plotly Pie Chart End
 
-    var layout = {
-    title: "Marker Size",
-    showlegend: false,
-    height: 400,
-    width: 480
-    };
+    //Plotly Line Chart Start
+        Plotly.d3.csv(
+          "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv",
+          function(err, rows) {
+            function unpack(rows, key) {
+              return rows.map(function(row) {
+                return row[key];
+              });
+            }
 
-    Plotly.newPlot("myDiv", data, layout);
+            var trace1 = {
+              type: "scatter",
+              mode: "lines",
+              name: "AAPL High",
+              x: unpack(rows, "Date"),
+              y: unpack(rows, "AAPL.High"),
+              line: { color: "#17BECF" }
+            };
+
+            var trace2 = {
+              type: "scatter",
+              mode: "lines",
+              name: "AAPL Low",
+              x: unpack(rows, "Date"),
+              y: unpack(rows, "AAPL.Low"),
+              line: { color: "#7F7F7F" }
+            };
+
+            var data = [trace1, trace2];
+
+            var layout = {
+              title: "Basic Time Series"
+            };
+
+            Plotly.newPlot("myDiv", data, layout);
+          }
+        );
+     //Plotly Line Chart Start
+     
+//*********************End Chart Input***************************
