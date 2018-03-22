e31427745ba3077e258d6ca3331bd2870a128ddb
Ktlewis17 <klewis.dpt@gmail.com>
2018-03-22 18:05:10 -0500

    working calendar html and js

--- assets/calendar/calendar.js
+++ assets/calendar/calendar.js
@@ -1,26 +1,15 @@
- $(document).ready(function(){
+$(function() {
 
-    var todoList = document.createElement("fullcalendar");
-    todoList.addEventListener('click', function() { 
-        console.log(todoList)
-           $("calendar").fullCalendar({
-             defaultView: "listWeek",
-         
-             // customize the button names,
-             // otherwise they'd all just say "list"
-             views: {
-               listDay: { buttonText: "list day" },
-               listWeek: { buttonText: "list week" },
-               listMonth: { buttonText: "list month" }
-                    },
-         
-             header: {
-               left: "title",
-               center: "",
-               right: "listDay,listWeek,listMonth"
-                     },
-             events: "https://fullcalendar.io/demo-events.json"
-                                        });
-         });
-       });
-        
+  $('#calendar').fullCalendar({
+    themeSystem: 'bootstrap4',
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay,listMonth'
+    },
+    weekNumbers: true,
+    eventLimit: true, // allow "more" link when too many events
+    events: 'https://fullcalendar.io/demo-events.json'
+  });
+
+});
