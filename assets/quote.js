9f4fc5bd02f45dc74a356c9d62330abb5040c6b5
Monir Cederstrom <32554823+monircederstrom@users.noreply.github.com>
2018-03-23 22:15:21 -0500

    fix CSS quote

--- assets/quote.js
+++ assets/quote.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $.ajax({
-        url: 'https://talaikis.com/api/quotes/random/',
+        url: 'http://api.adviceslip.com/advice',
         dataType: 'json',
         success: function (response) {
             console.log('success', response);
-            quote = response.quote;
-            $('#quote').text('"'+quote+'"');
+            advice = response.slip.advice;
+            $('#quote').text('"'+advice+'"');
             if (response.author) {
                 $('#author').text('- ' + response.author);
             } else {
